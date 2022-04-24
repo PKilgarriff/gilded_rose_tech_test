6ada5151e5cc480907bd9c21219d84a1f8bbe1a9
Paul <pkilgarriff@gmail.com>
2022-04-24 18:32:07 +0100

    Refactor Conditional block inside updateQuality
    - pull special cases out to top of block

--- src/shop.js
+++ src/shop.js
@@ -4,51 +4,25 @@ class Shop {
   }
   updateQuality() {
     this.items.forEach((item) => {
-      if (
-        item.name != "Aged Brie" &&
-        item.name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
+      if (item.name === "Sulfuras, Hand of Ragnaros") {
+        return;
+      } else if (item.name === "Aged Brie") {
+        item.sellIn = item.sellIn - 1;
+        item.quality = item.quality + 1;
+      } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+          item.quality = 0;
+        } else if (item.sellIn < 6) {
+          item.quality += 3;
+        } else if (item.sellIn < 11) {
+          item.quality += 2;
+        } else {
+          item.quality += 1;
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
         item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
+        item.quality = item.quality - 1;
       }
     });
 
--- test/shop.test.js
+++ test/shop.test.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const sinon = require("sinon");
+// const sinon = require("sinon");
 const Shop = require("../src/shop.js");
 const Item = require("../src/item.js");
 
@@ -21,11 +21,17 @@ describe("Shop", () => {
       gildedRose.updateQuality();
       expect(gildedRose.items[0].quality).to.equal(19);
     });
-    it("improves a standard item's quality property by 1", () => {
-      let items = [new Item("+5 Dexterity Vest", 10, 20)];
+    it("increases Aged Brie's quality property by 1", () => {
+      let items = [new Item("Aged Brie", 3, 0)];
       let gildedRose = new Shop(items);
       gildedRose.updateQuality();
-      expect(gildedRose.items[0].quality).to.equal(19);
+      expect(gildedRose.items[0].quality).to.equal(1);
+    });
+    it("decreases Aged Brie's sellIn property by 1", () => {
+      let items = [new Item("Aged Brie", 3, 0)];
+      let gildedRose = new Shop(items);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].sellIn).to.equal(2);
     });
     it("does not affect Legendary items", () => {
       let items = [new Item("Sulfuras, Hand of Ragnaros", 0, 80)];
