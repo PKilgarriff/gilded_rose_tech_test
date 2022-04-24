134c275645df037a7c1baeac63d8697c47f84364
Paul <pkilgarriff@gmail.com>
2022-04-24 19:01:37 +0100

    Refactor Shop test with describe blocks for categories of item

--- test/shop.test.js
+++ test/shop.test.js
@@ -9,38 +9,44 @@ describe("Shop", function () {
     expect(gildedRose.items).to.have.members([1, 2, 3]);
   });
   describe("updateQuality", function () {
-    it("reduces a standard item's sellIn property by 1", function () {
-      let items = [new Item("+5 Dexterity Vest", 10, 20)];
-      let gildedRose = new Shop(items);
-      gildedRose.updateQuality();
-      expect(gildedRose.items[0].sellIn).to.equal(9);
+    describe("standard items", function () {
+      it("reduces sellIn by 1", function () {
+        let items = [new Item("+5 Dexterity Vest", 10, 20)];
+        let gildedRose = new Shop(items);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(9);
+      });
+      it("reduces quality by 1", function () {
+        let items = [new Item("+5 Dexterity Vest", 10, 20)];
+        let gildedRose = new Shop(items);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].quality).to.equal(19);
+      });
     });
-    it("reduces a standard item's quality property by 1", function () {
-      let items = [new Item("+5 Dexterity Vest", 10, 20)];
-      let gildedRose = new Shop(items);
-      gildedRose.updateQuality();
-      expect(gildedRose.items[0].quality).to.equal(19);
+    describe("Aged Brie", function () {
+      it("increases quality by 1", function () {
+        let items = [new Item("Aged Brie", 3, 0)];
+        let gildedRose = new Shop(items);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].quality).to.equal(1);
+      });
+      it("decreases sellIn by 1", function () {
+        let items = [new Item("Aged Brie", 3, 0)];
+        let gildedRose = new Shop(items);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(2);
+      });
     });
-    it("increases Aged Brie's quality property by 1", function () {
-      let items = [new Item("Aged Brie", 3, 0)];
-      let gildedRose = new Shop(items);
-      gildedRose.updateQuality();
-      expect(gildedRose.items[0].quality).to.equal(1);
-    });
-    it("decreases Aged Brie's sellIn property by 1", function () {
-      let items = [new Item("Aged Brie", 3, 0)];
-      let gildedRose = new Shop(items);
-      gildedRose.updateQuality();
-      expect(gildedRose.items[0].sellIn).to.equal(2);
-    });
-    it("does not affect Legendary items", function () {
-      let items = [new Item("Sulfuras, Hand of Ragnaros", 0, 80)];
-      let gildedRose = new Shop(items);
-      gildedRose.updateQuality();
-      gildedRose.updateQuality();
-      gildedRose.updateQuality();
-      expect(gildedRose.items[0].sellIn).to.equal(0);
-      expect(gildedRose.items[0].quality).to.equal(80);
+    describe("Legendary Items", function () {
+      it("are not affected", function () {
+        let items = [new Item("Sulfuras, Hand of Ragnaros", 0, 80)];
+        let gildedRose = new Shop(items);
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(0);
+        expect(gildedRose.items[0].quality).to.equal(80);
+      });
     });
   });
 });
