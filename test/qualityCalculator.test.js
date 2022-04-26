9cfbeb54a3baa846a9b269dff64ae49475c42868
Paul <pkilgarriff@gmail.com>
2022-04-26 17:15:31 +0100

    Update test mocks
    - Mock Item with simple JS objects
    - Mock Quality Calculator with sinon
    - remove redundant tests

--- test/qualityCalculator.test.js
+++ test/qualityCalculator.test.js
@@ -1,63 +1,70 @@
 const { expect } = require("chai");
 const QualityCalculator = require("../src/qualityCalculator.js");
-const Item = require("../src/item.js");
 
 describe("Quality Calculator", function () {
   describe("calculate", function () {
     describe("standard items", function () {
       it("reduces quality by 1", function () {
-        let item = new Item("+5 Dexterity Vest", 10, 20);
+        let item = { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 };
         expect(QualityCalculator.calculate(item)).to.equal(19);
       });
     });
     describe("Aged Brie", function () {
       it("increases quality by 1", function () {
-        let item = new Item("Aged Brie", 3, 0);
+        let item = { name: "Aged Brie", sellIn: 3, quality: 0 };
         expect(QualityCalculator.calculate(item)).to.equal(1);
       });
     });
     describe("Legendary Items", function () {
       it("are not affected", function () {
-        let item = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+        let item = {
+          name: "Sulfuras, Hand of Ragnaros",
+          sellIn: 0,
+          quality: 80,
+        };
         expect(QualityCalculator.calculate(item)).to.equal(80);
       });
     });
     describe("Backstage Passes", function () {
       it("quality increases by 1 when there are more than 10 days to the show", function () {
-        let item = new Item(
-          "Backstage passes to a TAFKAL80ETC concert",
-          15,
-          20
-        );
+        let item = {
+          name: "Backstage passes to a TAFKAL80ETC concert",
+          sellIn: 15,
+          quality: 20,
+        };
         expect(QualityCalculator.calculate(item)).to.equal(21);
       });
       it("quality increases by 2 when there are less than 11 days to the show", function () {
-        let item = new Item(
-          "Backstage passes to a TAFKAL80ETC concert",
-          10,
-          20
-        );
+        let item = {
+          name: "Backstage passes to a TAFKAL80ETC concert",
+          sellIn: 10,
+          quality: 20,
+        };
         expect(QualityCalculator.calculate(item)).to.equal(22);
       });
       it("quality increases by 3 when there are less than 6 days to the show", function () {
-        let item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20);
+        let item = {
+          name: "Backstage passes to a TAFKAL80ETC concert",
+          sellIn: 5,
+          quality: 20,
+        };
         expect(QualityCalculator.calculate(item)).to.equal(23);
       });
       it("quality drops to 0 once the show has happened", function () {
-        let item = new Item(
-          "Backstage passes to a TAFKAL80ETC concert",
-          -1,
-          20
-        );
+        let item = {
+          name: "Backstage passes to a TAFKAL80ETC concert",
+          sellIn: -1,
+          quality: 20,
+        };
         expect(QualityCalculator.calculate(item)).to.equal(0);
       });
     });
     it("doesn't cause item quality to become negative", function () {
-      let item = new Item("+5 Dexterity Vest", 5, 0);
+      let item = { name: "+5 Dexterity Vest", sellIn: 5, quality: 0 };
       expect(QualityCalculator.calculate(item)).to.equal(0);
     });
     it("doesn't cause item quality to rise above 50", function () {
-      let item = new Item("Aged Brie", 10, 50);
+      let item = { name: "Aged Brie", sellIn: 10, quality: 50 };
       expect(QualityCalculator.calculate(item)).to.equal(50);
     });
   });
--- test/shop.test.js
+++ test/shop.test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
-// const sinon = require("sinon");
+const sinon = require("sinon");
 const Shop = require("../src/shop.js");
-const Item = require("../src/item.js");
+const QualityCalculator = require("../src/qualityCalculator.js");
 
 describe("Shop", function () {
   it("stores a list of items", function () {
@@ -13,93 +13,19 @@ describe("Shop", function () {
     expect(gildedRose.items).to.have.lengthOf(0);
   });
   describe("updateQuality", function () {
-    describe("standard items", function () {
-      it("reduces sellIn by 1", function () {
-        let gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
-        let items = gildedRose.updateQuality();
-        expect(items[0].sellIn).to.equal(9);
-      });
-      it("reduces quality by 1", function () {
-        let gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
-        let items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(19);
-      });
-    });
-    describe("Aged Brie", function () {
-      it("decreases sellIn by 1", function () {
-        let gildedRose = new Shop([new Item("Aged Brie", 3, 0)]);
-        let items = gildedRose.updateQuality();
-        expect(items[0].sellIn).to.equal(2);
-      });
-      it("increases quality by 1", function () {
-        let gildedRose = new Shop([new Item("Aged Brie", 3, 0)]);
-        let items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(1);
-      });
-    });
-    describe("Legendary Items", function () {
-      it("are not affected", function () {
-        let gildedRose = new Shop([
-          new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-        ]);
-        gildedRose.updateQuality();
-        gildedRose.updateQuality();
-        let items = gildedRose.updateQuality();
-        expect(items[0].sellIn).to.equal(0);
-        expect(items[0].quality).to.equal(80);
-      });
-    });
-    describe("Backstage Passes", function () {
-      it("decreases sellIn by 1", function () {
-        let gildedRose = new Shop([
-          new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-        ]);
-        let items = gildedRose.updateQuality();
-        expect(items[0].sellIn).to.equal(14);
-      });
-      it("quality increases by 1 when there are more than 10 days to the show", function () {
-        let gildedRose = new Shop([
-          new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-        ]);
-        let items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(21);
-      });
-      it("quality increases by 2 when there are less than 11 days to the show", function () {
-        let gildedRose = new Shop([
-          new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20),
-        ]);
-        let items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(22);
-      });
-      it("quality increases by 3 when there are less than 6 days to the show", function () {
-        let gildedRose = new Shop([
-          new Item("Backstage passes to a TAFKAL80ETC concert", 6, 20),
-        ]);
-        let items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(23);
-      });
-      it("quality drops to 0 once the show has happened", function () {
-        let gildedRose = new Shop([
-          new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20),
-        ]);
-        let items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(0);
-      });
-    });
-    it("doesn't cause item quality to become negative", function () {
-      let gildedRose = new Shop([new Item("+5 Dexterity Vest", 5, 3)]);
-      gildedRose.updateQuality();
-      gildedRose.updateQuality();
-      gildedRose.updateQuality();
-      let items = gildedRose.updateQuality();
-      expect(items[0].quality).to.equal(0);
-    });
-    it("doesn't cause item quality to rise above 50", function () {
-      let gildedRose = new Shop([new Item("Aged Brie", 10, 48)]);
-      gildedRose.updateQuality();
-      gildedRose.updateQuality();
-      let items = gildedRose.updateQuality();
-      expect(items[0].quality).to.equal(50);
+    it("calls the calculate method of QualityCalculator for each item", function () {
+      let item = { name: "example", sellIn: 5, quality: 6 };
+      let calculatorStub = sinon.stub(QualityCalculator);
+      calculatorStub.isLegendary.returns(false);
+      calculatorStub.calculate
+        .withArgs(item)
+        .onFirstCall()
+        .returns("The example item has been calculated");
+      let gildedRose = new Shop([item], calculatorStub);
+      let updatedItems = gildedRose.updateQuality();
+      expect(updatedItems[0].quality).to.contain(
+        "The example item has been calculated"
+      );
     });
   });
 });
